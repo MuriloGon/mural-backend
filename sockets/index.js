fdbfadfa2a87ddd0c0a56d865b7468a7d2dadae3
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-10-25 17:37:52 -0300

    server messages updated

--- sockets/index.js
+++ sockets/index.js
@@ -2,4 +2,9 @@
 const socketio = require('socket.io');
 
 module.exports = (io) => io.on('connection', (socket) => {
+  socket.emit('serverMessage', 'Hello, wellcome to our chat!');
+  socket.broadcast.emit('serverMessage', `OW! ${socket.id} is already connected! :D`);
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('serverMessage', `OH! ${socket.id} We lost a warrior! :(`);
+  });
 }); 
