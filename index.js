2e6228f280a298e4ef576b60a1cb6f490d66f512
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-10-25 17:27:05 -0300

    primeira config dos sockets

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: '*' }));
 
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST'], 
+  }});
+
+io.on('connection', (socket) => {
+  console.log(`User connected. ID: ${socket.id} `);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}`));
